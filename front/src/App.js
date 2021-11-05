ef982912a695c6b2588d69eb52866788bc480a6e
“깃 <hy04730@naver.com>
2021-11-05 13:51:01 +0900

    feat: Google Login(FE)

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,10 @@
-import GoogleLogin from 'react-google-login';
+import Login from './components/common/navBar/login/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <GoogleLogin
-        clientId={'813123317206-o4mo2eas1kbve8e0914g5uev9ru27he9.apps.googleusercontent.com'}>
-        구글로 로그인</GoogleLogin>
+      <Login />
     </div>
   );
 }
---
+++ front/src/components/common/navBar/login/Login.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+import axios from "axios";
+import styled from "styled-components";
+
+import google from "../../../../images/icons/google.png";
+
+const Login = ({ ...props }) => {
+  const history = useHistory();
+
+  const onSuccess = async (response) => {
+    const {
+      profileObj: { googleId, email, name, imageUrl },
+    } = response;
+
+    try {
+      const res = await axios.get(`/users/${googleId}`);
+
+      if (res.status === 204) {
+        const user = {
+          id: googleId,
+          name,
+          email,
+          imgUrl: imageUrl,
+        };
+
+        await axios.post("/users", user);
+
+        await axios.get(`users/${user.id}`);
+
+        localStorage.setItem("user_id", user.id);
+        localStorage.setItem("user_image", user.imgUrl);
+        props.onCancel();
+        history.go(0);
+      } else {
+        localStorage.setItem("user_id", res.data.id);
+        localStorage.setItem("user_image", res.data.imgUrl);
+        props.onCancel();
+        history.go(0);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onFailure = (response) => {
+    console.log("Login Failed: ", response);
+  };
+
+  console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
+  return (
+    <>
+      <GoogleLogin
+        clientId={
+          "813123317206-o4mo2eas1kbve8e0914g5uev9ru27he9.apps.googleusercontent.com"
+        }
+        render={(renderProps) => (
+          <GoogleLoginButton
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            <GoogleIcon>
+              <img src={google} alt="Google icon" />
+            </GoogleIcon>
+            구글로 로그인
+          </GoogleLoginButton>
+        )}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+      />
+    </>
+  );
+};
+
+export default Login;
+
+const GoogleIcon = styled.span`
+  position: absolute;
+  left: 14px;
+`;
+
+const GoogleLoginButton = styled.button`
+  width: 300px;
+  height: 50px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #ffffff;
+  background-color: #db4437;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+  text-align: center;
+  cursor: pointer;
+  position: relative;
+`;
