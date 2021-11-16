d2d0a60cdc488f63f90674637368629aed387909
hcs4125 <hcs4125@gmail.com>
2021-11-16 12:42:24 +0900

    feat: MemberController - memberApply 구현

--- back/src/main/java/org/moDgo/controller/member/MemberController.java
+++ back/src/main/java/org/moDgo/controller/member/MemberController.java
@@ -2,8 +2,12 @@ package org.moDgo.controller.member;
 
 
 import lombok.RequiredArgsConstructor;
+import org.moDgo.domain.Member;
 import org.moDgo.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+
+    //상세페이지에서 참여신청으로 해당 REST API 사용
+    @PostMapping
+    public ResponseEntity<MemberResponseDto> memberApply(
+            @RequestBody MemberCreateRequestDto memberCreateRequestDto
+    ) {
+        try {
+            Member member = memberService.apply(memberCreateRequestDto);
+            return new ResponseEntity("참여신청이 완료되었습니다." + member.getId(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity("이미 참여신청하셨습니다.", HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
--- back/src/main/java/org/moDgo/service/MemberService.java
+++ back/src/main/java/org/moDgo/service/MemberService.java
@@ -2,9 +2,14 @@ package org.moDgo.service;
 
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.moDgo.common.error.ClubNotFoundException;
+import org.moDgo.common.error.UserNotFoundException;
 import org.moDgo.controller.member.MemberApproveRequestDto;
 import org.moDgo.controller.member.MemberCreateRequestDto;
+import org.moDgo.domain.ApprovalStatus;
+import org.moDgo.domain.Club;
 import org.moDgo.domain.Member;
+import org.moDgo.domain.User;
 import org.moDgo.repository.ClubRepository;
 import org.moDgo.repository.MemberRepository;
 import org.moDgo.repository.UserRepository;
@@ -22,7 +27,13 @@ public class MemberService {
 
     @Transactional
     public Member apply(MemberCreateRequestDto memberCreateRequestDto) {
-        return new Member();
+        User user = userRepository.findById(memberCreateRequestDto.getUserId()).orElseThrow(UserNotFoundException::new);
+        Club club = clubRepository.findById(memberCreateRequestDto.getClubId()).orElseThrow(ClubNotFoundException::new);
+        if (memberRepository.findByUserAndClub(user, club).isPresent()) {
+            return null;
+        }
+        Member member = Member.builder().user(user).club(club).approvalStatus(ApprovalStatus.WAITING).build();
+        return memberRepository.save(member);
     }
 
     @Transactional
