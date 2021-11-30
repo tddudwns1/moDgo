0235c637f25a5d5b871b66d89ff430047d931a49
hcs4125 <hcs4125@gmail.com>
2021-11-30 22:38:44 +0900

    feat: Member Evaluation score get

---
+++ back/src/main/java/org/moDgo/controller/member/MemberEvaluationResponseDto.java
@@ -0,0 +1,23 @@
+package org.moDgo.controller.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.moDgo.domain.EvaluationStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberEvaluationResponseDto {
+    int goodScore;
+    int badScore;
+    int normalScore;
+    EvaluationStatus evaluationStatus;
+
+    MemberEvaluationResponseDto(int badScore, int goodScore, int normalScore, EvaluationStatus evaluationStatus) {
+        this.badScore = badScore;
+        this.evaluationStatus = evaluationStatus;
+        this.goodScore = goodScore;
+        this.normalScore = normalScore;
+    }
+}
