54b8951cd23bfce739498fe5473c9dad574955b2
“깃 <hy04730@naver.com>
2021-12-03 22:51:08 +0900

    fix: 상세페이지 참여회원 수정

--- front/src/components/detail/DetailInfo.jsx
+++ front/src/components/detail/DetailInfo.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useState } from 'react';
-import { Divider, Button, Modal, message } from 'antd';
-import styled from 'styled-components';
-import { customMedia } from '../../GlobalStyles';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Divider, Button, Modal, message } from "antd";
+import styled from "styled-components";
+import { customMedia } from "../../GlobalStyles";
 
 const DetailInfo = ({ ...props }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [memberId, setMemberId] = useState(0);
-  const [memberName, setMemberName] = useState('');
+  const [memberName, setMemberName] = useState("");
   const [voteCount, setVoteCount] = useState(0);
-  const userId = localStorage.getItem('user_id');
+  const userId = localStorage.getItem("user_id");
   const confirmedIdArr = [];
   const [getEvaluation, setGetEvaluation] = useState();
-  let evaluationStatus = '';
+  let evaluationStatus = "";
   let memberIdStorage = 0;
 
   useEffect(() => {
@@ -42,20 +42,20 @@ const DetailInfo = ({ ...props }) => {
 
     try {
       const res = await axios.post(
-        process.env.REACT_APP_API_URL + '/members/evaluation',
+        process.env.REACT_APP_API_URL + "/members/evaluation",
         JSON.stringify(data),
         {
           headers: {
-            'Content-Type': `application/json`,
+            "Content-Type": `application/json`,
           },
         }
       );
       console.log(res);
 
       if (res.status === 200) {
-        message.success('평가가 완료되었습니다.');
+        message.success("평가가 완료되었습니다.");
       } else {
-        message.error('평가가 실패했습니다.');
+        message.error("평가가 실패했습니다.");
       }
     } catch (err) {
       console.log(err);
@@ -80,6 +80,7 @@ const DetailInfo = ({ ...props }) => {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
+
   if (userId) {
     for (let i = 0; i < props.confirmedUser.length; i++) {
       confirmedIdArr.push(props.confirmedUser[i].userId);
@@ -94,102 +95,101 @@ const DetailInfo = ({ ...props }) => {
         <Contents>{props.club.description}</Contents>
       </TextBox>
       <Title>참여 회원</Title>
-      {(() => {
-        if (userId) {
-          <TextBox>
-            <Contents>
-              {props.confirmedUser.map((user, i) => (
-                <img
-                  src={props.confirmedUser[i].imgUrl}
-                  alt="User profile"
-                  key={i}
-                  onClick={() => {
-                    setMemberId(props.confirmedUser[i].id);
-                    setMemberName(props.confirmedUser[i].name);
-                    console.log(memberId);
-                    console.log(i);
-                    console.log(props.confirmedUser[i].id);
-                    console.log(props.confirmedUser[i].name);
+      <TextBox>
+        <Contents>
+          {confirmedIdArr.includes(userId) ? (
+            props.confirmedUser.map((user, i) => (
+              <img
+                src={props.confirmedUser[i].imgUrl}
+                alt="User profile"
+                key={i}
+                onClick={() => {
+                  setMemberId(props.confirmedUser[i].id);
+                  setMemberName(props.confirmedUser[i].name);
+                  console.log(memberId);
+                  console.log(i);
+                  console.log(props.confirmedUser[i].id);
+                  console.log(props.confirmedUser[i].name);
 
-                    // handleGetEvaluation(memberId);
+                  // handleGetEvaluation(memberId);
 
-                    // console.log(getEvaluation);
-                    // console.log("handleGetEvaluation");
-                    // console.log(getEvaluation);
+                  // console.log(getEvaluation);
+                  // console.log("handleGetEvaluation");
+                  // console.log(getEvaluation);
 
-                    // console.log("evaluation");
-                    // console.log(getEvaluation);
-                    // console.log(props.getEvaluation.evaluationStatus);
-                    showModal();
-                  }}
-                />
-              ))}
-              {confirmedIdArr.includes(userId) ? (
-                voteCount !== 1 ? (
-                  <StyledModal visible={isModalVisible} onCancel={handleCancel}>
-                    <Title>
-                      <strong>{memberName}님 회원 평가</strong>
-                      <hr />
-                      <h1
-                        onClick={() => {
-                          getData(memberId);
-                          handleGetEvaluation(memberId);
-                          if (getEvaluation.evaluationStatus !== 'CLEAR') {
-                            handleEvaluation('GOOD', memberId);
-                            handleGetEvaluation(memberId);
-                            handleCancel();
-                          } else message.warning('이미 평가 완료했습니다.');
-                          handleGetEvaluation(memberId);
-                        }}
-                      >
-                        😍
-                      </h1>
-
-                      <h1
-                        onClick={() => {
-                          getData(memberId);
-                          handleGetEvaluation(memberId);
-                          if (getEvaluation.evaluationStatus !== 'CLEAR') {
-                            handleEvaluation('NORMAL', memberId);
-                            handleGetEvaluation(memberId);
-                            handleCancel();
-                          } else message.warning('이미 평가 완료했습니다.');
-                          handleGetEvaluation(memberId);
-                        }}
-                      >
-                        🙂
-                      </h1>
-
-                      <h1
-                        onClick={() => {
-                          getData(memberId);
-                          handleGetEvaluation(memberId);
-                          if (getEvaluation.evaluationStatus !== 'CLEAR') {
-                            handleEvaluation('BAD', memberId);
-                            handleGetEvaluation(memberId);
-                            handleCancel();
-                          } else message.warning('이미 평가 완료했습니다.');
-                        }}
-                      >
-                        🙁
-                      </h1>
-                    </Title>
-                    <SubmitButton
-                      onClick={() => {
+                  // console.log("evaluation");
+                  // console.log(getEvaluation);
+                  // console.log(props.getEvaluation.evaluationStatus);
+                  showModal();
+                }}
+              />
+            ))
+          ) : (
+            <Contents>참여 회원은 해당 모임에 참여중일 때만 보입니다.</Contents>
+          )}
+          {confirmedIdArr.includes(userId) ? (
+            voteCount !== 1 ? (
+              <StyledModal visible={isModalVisible} onCancel={handleCancel}>
+                <Title>
+                  <strong>{memberName}님 회원 평가</strong>
+                  <hr />
+                  <h1
+                    onClick={() => {
+                      getData(memberId);
+                      handleGetEvaluation(memberId);
+                      if (getEvaluation.evaluationStatus !== "CLEAR") {
+                        handleEvaluation("GOOD", memberId);
+                        handleGetEvaluation(memberId);
                         handleCancel();
-                      }}
-                    >
-                      확인
-                    </SubmitButton>
-                  </StyledModal>
-                ) : null
-              ) : null}
-            </Contents>
-          </TextBox>;
-        } else {
-          return <contents>로그인 전에는 멤버 확인이 불가능합니다.</contents>;
-        }
-      })()}
+                      } else message.warning("이미 평가 완료했습니다.");
+                      handleGetEvaluation(memberId);
+                    }}
+                  >
+                    😍
+                  </h1>
+
+                  <h1
+                    onClick={() => {
+                      getData(memberId);
+                      handleGetEvaluation(memberId);
+                      if (getEvaluation.evaluationStatus !== "CLEAR") {
+                        handleEvaluation("NORMAL", memberId);
+                        handleGetEvaluation(memberId);
+                        handleCancel();
+                      } else message.warning("이미 평가 완료했습니다.");
+                      handleGetEvaluation(memberId);
+                    }}
+                  >
+                    🙂
+                  </h1>
+
+                  <h1
+                    onClick={() => {
+                      getData(memberId);
+                      handleGetEvaluation(memberId);
+                      if (getEvaluation.evaluationStatus !== "CLEAR") {
+                        handleEvaluation("BAD", memberId);
+                        handleGetEvaluation(memberId);
+                        handleCancel();
+                      } else message.warning("이미 평가 완료했습니다.");
+                    }}
+                  >
+                    🙁
+                  </h1>
+                </Title>
+                <SubmitButton
+                  onClick={() => {
+                    handleCancel();
+                  }}
+                >
+                  확인
+                </SubmitButton>
+              </StyledModal>
+            ) : null
+          ) : null}
+        </Contents>
+      </TextBox>
+
       <Divider />
     </DetailInfoContainer>
   );
@@ -204,13 +204,13 @@ const StyledModal = styled(Modal)`
     padding: 30px 55px;
     display: flex;
     align-items: center;
-    ${customMedia.lessThan('mobile')`
+    ${customMedia.lessThan("mobile")`
       padding: 10px;
     `}
-    ${customMedia.between('mobile', 'largeMobile')`
+    ${customMedia.between("mobile", "largeMobile")`
       padding: 10px 15px;
     `}
-    ${customMedia.between('largeMobile', 'tablet')`
+    ${customMedia.between("largeMobile", "tablet")`
       padding: 20px 35px;
     `}
   }
@@ -220,14 +220,14 @@ const StyledModal = styled(Modal)`
     justify-content: center;
     align-items: center;
     gap: 48px;
-    ${customMedia.lessThan('mobile')`
+    ${customMedia.lessThan("mobile")`
       gap: 24px;
       padding: 15px;
     `}
-    ${customMedia.between('mobile', 'largeMobile')`
+    ${customMedia.between("mobile", "largeMobile")`
       gap: 26px;
     `}
-    ${customMedia.between('largeMobile', 'tablet')`
+    ${customMedia.between("largeMobile", "tablet")`
       gap: 32px;
     `}
   }
@@ -249,11 +249,11 @@ const SubmitButton = styled.button`
   text-align: center;
   cursor: pointer;
   position: relative;
-  ${customMedia.lessThan('mobile')`
+  ${customMedia.lessThan("mobile")`
     font-size: 14px;
     width: 220px;
   `}
-  ${customMedia.between('mobile', 'tablet')`
+  ${customMedia.between("mobile", "tablet")`
     font-size: 16px;
     width: 240px;
   `}
@@ -272,16 +272,16 @@ const Title = styled.div`
     margin: auto;
   }
 
-  ${customMedia.lessThan('mobile')`
+  ${customMedia.lessThan("mobile")`
     font-size: 18px;
   `}
-  ${customMedia.between('mobile', 'largeMobile')`
+  ${customMedia.between("mobile", "largeMobile")`
     font-size: 18px;
   `}
-	${customMedia.between('largeMobile', 'tablet')`
+	${customMedia.between("largeMobile", "tablet")`
     font-size: 20px;
   `}
-	${customMedia.between('tablet', 'desktop')`
+	${customMedia.between("tablet", "desktop")`
     font-size: 22px;
   `}
 `;
@@ -291,16 +291,16 @@ const SubTitle = styled.div`
   font-weight: 600;
   margin-bottom: 15px;
 
-  ${customMedia.lessThan('mobile')`
+  ${customMedia.lessThan("mobile")`
     font-size: 16px;
   `}
-  ${customMedia.between('mobile', 'largeMobile')`
+  ${customMedia.between("mobile", "largeMobile")`
     font-size: 16px;
   `}
-	${customMedia.between('largeMobile', 'tablet')`
+	${customMedia.between("largeMobile", "tablet")`
     font-size: 18px;
   `}
-	${customMedia.between('tablet', 'desktop')`
+	${customMedia.between("tablet", "desktop")`
     font-size: 18px;
   `}
 `;
@@ -314,16 +314,16 @@ const Contents = styled.div`
     margin: auto 20px auto 20px;
   }
 
-  ${customMedia.lessThan('mobile')`
+  ${customMedia.lessThan("mobile")`
     font-size: 14px;
   `}
-  ${customMedia.between('mobile', 'largeMobile')`
+  ${customMedia.between("mobile", "largeMobile")`
     font-size: 14px;
   `}
-	${customMedia.between('largeMobile', 'tablet')`
+	${customMedia.between("largeMobile", "tablet")`
     font-size: 14px;
   `}
-	${customMedia.between('tablet', 'desktop')`
+	${customMedia.between("tablet", "desktop")`
     font-size: 14px;
   `}
 `;
@@ -341,15 +341,15 @@ const NavProfile = styled.div`
     height: 100%;
   }
   margin-bottom: 40px;
-  ${customMedia.lessThan('mobile')`
+  ${customMedia.lessThan("mobile")`
     width: 28px;
     height: 28px;
   `}
-  ${customMedia.between('mobile', 'largeMobile')`
+  ${customMedia.between("mobile", "largeMobile")`
     width: 30px;
     height: 30px;
   `}
-	${customMedia.between('largeMobile', 'tablet')`
+	${customMedia.between("largeMobile", "tablet")`
     width: 32px;
     height: 32px;
   `}
